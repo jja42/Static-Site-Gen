1987901d1470ec87d221c1d91df5286aee906e6c
jja42 <jja42w@gmail.com>
2025-06-19 18:48:38 -0400

    Markdown to HTML

--- src/markdown.py
+++ src/markdown.py
@@ -1,5 +1,5 @@
-import textnode
-import htmlnode
+from textnode import *
+from htmlnode import *
 from enum import Enum
 import re
 
@@ -12,12 +12,11 @@ class BlockType(Enum):
     ORDERED_LIST = "ordered_list"
 
 def markdown_to_blocks(markdown):
-
     blocks = []
     paragraphs = markdown.split("\n\n")
 
     for paragraph in paragraphs:
-        if(paragraph.strip("\n") == ""):
+        if(paragraph == ""):
             continue
         paragraph = paragraph.strip()
         blocks.append(paragraph)
@@ -64,3 +63,97 @@ def block_to_block_type(markdown_block):
         return BlockType.ORDERED_LIST
     
     return BlockType.PARAGRAPH
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    parent_node = ParentNode("div",children)
+    return parent_node
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    match block_type:
+        case BlockType.PARAGRAPH:
+            block_node = markdown_paragraph_block_to_node(block)
+        case BlockType.HEADING:
+            block_node = markdown_heading_block_to_node(block)
+        case BlockType.CODE:
+            block_node = markdown_code_block_to_node(block)
+        case BlockType.QUOTE:
+            block_node = markdown_quote_block_to_node(block)
+        case BlockType.UNORDERED_LIST:
+            block_node = markdown_unordered_block_to_node(block)
+        case BlockType.ORDERED_LIST:
+            block_node = markdown_ordered_block_to_node(block)
+    return block_node
+
+def markdown_paragraph_block_to_node(block):
+    lines = block.split("\n")
+    block = " ".join(lines)
+    child_nodes = text_to_children(block)
+    block_node = ParentNode("p",child_nodes)
+    return block_node
+
+def markdown_heading_block_to_node(block):
+    match = re.search(r'^(#+)\s', block)
+    if match:
+        heading_level = len(match.group(1))
+        heading_level = f"h{heading_level}"
+    block = block.replace("#","")
+    block = block.lstrip()
+    child_nodes = text_to_children(block)
+    block_node = ParentNode(heading_level,child_nodes)
+    return block_node
+
+def markdown_code_block_to_node(block):
+    block = block.replace("```","")
+    block = block.lstrip("\n")
+    node = TextNode(block,TextType.TEXT)
+    html_node = text_node_to_html_node(node)
+    block_node = ParentNode("code",[html_node])
+    wrapper_node = ParentNode("pre",[block_node])
+    return wrapper_node
+
+def markdown_quote_block_to_node(block):
+    lines = block.split("\n")
+    quotes = []
+    for line in lines:
+        quotes.append(line.lstrip(">").strip())
+    block = " ".join(quotes)
+    child_nodes = text_to_children(block)
+    block_node = ParentNode("blockquote",child_nodes)
+    return block_node
+
+def markdown_unordered_block_to_node(block):
+    lines = block.split("\n")
+    child_nodes = []
+    for line in lines:
+        text = line[2:]
+        children = text_to_children(text)
+        child_nodes.append(ParentNode("li", children))
+    block_node = ParentNode("ul",child_nodes)
+    return block_node
+
+def markdown_ordered_block_to_node(block):
+    lines = block.split("\n")
+    child_nodes = []
+    for line in lines:
+        text = line[3:]
+        children = text_to_children(text)
+        child_nodes.append(ParentNode("li", children))
+    block_node = ParentNode("ol",child_nodes)
+    return block_node
+    
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    htmlnodes = []
+    for node in text_nodes:
+        new_node = text_node_to_html_node(node)
+        htmlnodes.append(new_node)
+    return htmlnodes
+
+
